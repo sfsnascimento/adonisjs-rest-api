dba4db08401a3c286c659feacc174538edb1edc7
Samuel Nascimento <sfsnascimento@gmail.com>
2024-07-29 18:24:19 -0300

    criando model product

---
+++ app/models/product.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Product extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare user_id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare price: number
+
+  @column()
+  declare quantity: number
+
+  @column()
+  declare active: boolean
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
